0b05327b95ab1559a628a605f5485437a6d865de
kisho05 <118363067+kisho05@users.noreply.github.com>
2023-12-08 11:46:19 +0530

    UPDATE

---
+++ CustomerRegistrationApp/src/main/java/com/example/CustomerRegistrationApp/CustomerRegistrationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomerRegistrationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegistrationAppApplication.class, args);
+	}
+
+}
